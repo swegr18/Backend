d0dd119761f338154e3ee42f64371a057bfd4afc
Richie <rad75@bath.ac.uk>
2026-03-02 13:39:13 +0000

    Tidied up code a bit

--- metrics.py
+++ metrics.py
@@ -16,29 +16,17 @@ def all_metrics(path):
 
     whisperOutput = transcription(path)
     text = whisperOutput.get("text")
-
-
     y, sr = librosa.load(path)
     duration = librosa.get_duration(y=y,sr=sr)
+
     #volume calculations
-    rms = librosa.feature.rms(y=y)
-    db = librosa.amplitude_to_db(rms, ref=1.0)
-    average_db = float(np.mean(db))
-    average_db = np.round(average_db,1)
+    average_db = calculate_average_volume(y)
+
     #wpm
-    word_count = len(text.split())
-    wpm = word_count/(duration/60)
-    wpm = np.round(wpm,1)
+    wpm = calculate_wpm(text, duration)
+
     #pitch calculations and background noise trimming
-    y, _ = librosa.effects.trim(y)
-    y_harmonic, _ = librosa.effects.hpss(y)
-    f0 = librosa.yin(y_harmonic,fmin=80,fmax=400)
-    f0 = f0[~np.isnan(f0)]
-    avg_freq = float(np.mean(f0))
-    avg_freq = np.round(avg_freq,1)
-    frames_per_second = int(len(f0) / librosa.get_duration(y=y, sr=sr))
-    chunks = np.array_split(f0, frames_per_second)
-    avg_freq_second = [np.mean(chunk) for chunk in chunks]
+    avg_freq, avg_freq_second = calculate_avg_pitch(y, sr)
 
     #filler words
     fillerProportion = filler.calculateFillerProportion(text)
@@ -109,7 +97,7 @@ def calc_wpm_live(session_wpm,session_lock,session_id: str, chunk_index: int, ch
 # Calculate proportion of transcribable text (Whisper's confidence in the transcription)
 #
 # Input: Dict returned by model.transcribe()
-# Output: Proportion of transcribable text
+# Output: Float Proportion of transcribable text
 #
 def calculateTranscribability(whisperOutput):
     segments = whisperOutput.get("segments")
@@ -121,3 +109,42 @@ def calculateTranscribability(whisperOutput):
 
     return math.exp(totalLog)
         
+# Calculate average volume in deciBels
+#
+# Input: Librosa audio time series (np.ndarray)
+# Output: Float, in deciBels
+#
+def calculate_average_volume(y):
+    rms = librosa.feature.rms(y=y)
+    db = librosa.amplitude_to_db(rms, ref=1.0)
+    average_db = float(np.mean(db))
+    return np.round(average_db,1)
+
+# Calculate words per minute
+#
+# Input: text string, duration as float
+# Output: Float, words per minute
+#
+def calculate_wpm(text, duration):
+    word_count = len(text.split())
+    wpm = word_count/(duration/60)
+    return np.round(wpm,1)
+
+# Calculate average pitch
+#
+# Input: Librosa time series, sample rate
+# Output: Tuple of average pitch frequency and average frequency per second?
+#
+def calculate_avg_pitch(y, sr):
+    y, _ = librosa.effects.trim(y)
+    y_harmonic, _ = librosa.effects.hpss(y)
+    f0 = librosa.yin(y_harmonic,fmin=80,fmax=400)
+    f0 = f0[~np.isnan(f0)]
+    avg_freq = float(np.mean(f0))
+    avg_freq = np.round(avg_freq,1)
+    frames_per_second = int(len(f0) / librosa.get_duration(y=y, sr=sr))
+    chunks = np.array_split(f0, frames_per_second)
+    avg_freq_second = [np.mean(chunk) for chunk in chunks]
+
+    return avg_freq, avg_freq_second
+    
